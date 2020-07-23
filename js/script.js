db6873aa8ceed788c0c7f3f1d0de812cc566fa0a
Michele Curtaz <mcurtaz@gmail.com>
2020-07-23 19:43:38 +0200

    Print Holidays

--- js/script.js
+++ js/script.js
@@ -7,11 +7,14 @@ function init() {
 
   printDayOfMonth(currentMonth);
 
+  ajaxHoliday(currentMonth);
+
+  
 }
 
-function printDayOfMonth(month) {
+function printDayOfMonth(currentMonth) {
 
-  var numOfDays = month.daysInMonth();
+  var numOfDays = currentMonth.daysInMonth();
   var template = $("#li-template").html();
   var compiled = Handlebars.compile(template)
   var target = $("#days-of-month>ul");
@@ -19,14 +22,49 @@ function printDayOfMonth(month) {
 
   for (var i = 1; i <= numOfDays; i++) {
 
-    var dataComplete = moment({year: month.year(), month: month.month(), day: i})
+    var dataComplete = moment({year: currentMonth.year(), month: currentMonth.month(), day: i})
 
     var newLi = compiled({
-      "data-day": dataComplete.format("DD-MM-YYYY"),
+      "data-day": dataComplete.format("YYYY-MM-DD"),
       "n-day": dataComplete.format("D"),
       "dow": dataComplete.format("dddd")
     });
 
     target.append(newLi);
+  }
+}
+
+function ajaxHoliday(currentMonth) {
+  $.ajax({
+    url: "https://flynn.boolean.careers/exercises/api/holidays",
+    method: "GET",
+    data: {
+      "year": currentMonth.year(),
+      "month": currentMonth.month()
+    },
+    success: function (data) {
+      if(data["success"]){
+
+        printHoliday(data["response"]);
+      } else {
+        alert("Impossibile trovare dati festività per questo mese!");
+      }
+    } ,
+    error: function (err) {
+      console.log("err", err);
+    }
+
+  });
+}
+
+function printHoliday(arrayHoliday) {
+
+  for (var i = 0; i < arrayHoliday.length; i++) {
+    var dateHoliday = arrayHoliday[i]["date"];
+    var nameHoliday = arrayHoliday[i]["name"];
+    var target = $("#days-of-month [data-day=\"" + dateHoliday + "\"]");
+    target.addClass("festivity");
+    target.find(".f-name").text(nameHoliday);
+
   }
 }
